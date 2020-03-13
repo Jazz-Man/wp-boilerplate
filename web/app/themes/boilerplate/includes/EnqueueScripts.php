82560b3d2e007e065cdb2a9608333814943efd9f
Vasyl Sokolik <vsokolyk@gmail.com>
2020-03-13 14:38:15 +0200

    update wp_enqueue_scripts

--- web/app/themes/boilerplate/includes/EnqueueScripts.php
+++ web/app/themes/boilerplate/includes/EnqueueScripts.php
@@ -2,7 +2,9 @@
 
 namespace JazzMan\Themes;
 
+use _WP_Dependency;
 use JazzMan\AutoloadInterface\AutoloadInterface;
+use JazzMan\Performance\Optimization\Enqueue;
 
 /**
  * Class EnqueueScripts.
@@ -13,10 +15,22 @@ class EnqueueScripts implements AutoloadInterface
     {
         add_action('wp_enqueue_scripts', [$this, 'cdn_scripts']);
         add_action('wp_enqueue_scripts', [$this, 'enqueue_scripts']);
+//        add_action('wp_print_styles', [$this, 'remove_assets']);
     }
 
     public function enqueue_scripts()
     {
+        $grid_css_file = app_dir_path('dist/css/bootstrap-grid.css');
+
+        if (file_exists($grid_css_file)) {
+            $grid_css_current_file_time = file_get_contents($grid_css_file);
+
+            wp_register_style('wp-boilerplate-grid', false); // phpcs:ignore
+            wp_enqueue_style('wp-boilerplate-grid');
+
+            wp_add_inline_style('iconic-grid', $grid_css_current_file_time);
+        }
+
         wp_enqueue_style('wp-boilerplate-style', app_url_path('dist/css/main.css'));
 
         wp_enqueue_script('wp-boilerplate-script', app_url_path('dist/js/main.js'));
@@ -24,49 +38,52 @@ class EnqueueScripts implements AutoloadInterface
 
     public function cdn_scripts()
     {
-        wp_deregister_script('jquery-core');
-        wp_deregister_script('jquery');
-
-        wp_register_script('jquery-core', 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js', false,
-            null, true);
-        wp_register_script('jquery', false, ['jquery-core'], null, true);
-
-        $cdn_libs = [
-            'lodash' => 'https://cdn.jsdelivr.net/npm/lodash@4.17.11/lodash.min.js',
-            'moment' => 'https://cdn.jsdelivr.net/npm/moment@2.22.2/moment.min.js',
-            'react' => 'https://cdn.jsdelivr.net/npm/react@16.8.4/umd/react.production.min.js',
-            'react-dom' => 'https://cdn.jsdelivr.net/npm/react-dom@16.8.4/umd/react-dom.production.min.js',
-        ];
-
-        foreach ($cdn_libs as $handle => $url) {
-            $this->deregister_script($handle, $url);
-        }
-    }
-
-    /**
-     * @param string      $handle
-     * @param string|null $new_url
-     */
-    private function deregister_script(string $handle, $new_url = null)
-    {
         $registered_scripts = wp_scripts()->registered;
 
-        if (!empty($registered_scripts[$handle])) {
-            /** @var \_WP_Dependency $js_lib */
-            $js_lib = $registered_scripts[$handle];
+        $libs = [
+            'lodash',
+            'moment',
+            'react',
+            'react-dom',
+        ];
 
-            wp_dequeue_script($js_lib->handle);
-            wp_deregister_script($js_lib->handle);
+        $cdn_base_url = 'https://cdn.jsdelivr.net/npm';
 
-            if (!empty($new_url)) {
-                wp_register_script($js_lib->handle, $new_url, $js_lib->deps, $js_lib->ver, true);
+        $cdn_libs = [];
 
-                if (!empty($js_lib->extra) && \is_array($js_lib->extra)) {
-                    foreach ($js_lib->extra as $position => $data) {
-                        wp_add_inline_script($js_lib->handle, end($data), $position);
-                    }
-                }
+        foreach ($libs as $lib) {
+            if (!empty($registered_scripts[$lib]) && $registered_scripts[$lib] instanceof _WP_Dependency && $registered_scripts[$lib]->ver) {
+                $is_react = \in_array($lib, ['react', 'react-dom']);
+
+                $lib_name = $is_react ? '.production' : '';
+                $lib_prefix = $is_react ? 'umd/' : '';
+
+                $cdn_libs[$lib] = "{$cdn_base_url}/{$lib}@{$registered_scripts[$lib]->ver}/{$lib_prefix}{$lib}{$lib_name}.min.js";
+            }
+        }
+
+        if (!empty($cdn_libs)) {
+            foreach ($cdn_libs as $handle => $url) {
+                Enqueue::deregisterScript($handle, $url);
             }
         }
     }
+
+    public function remove_assets()
+    {
+        $deregister_styles = [
+        ];
+
+        $deregister_scripts = [
+        ];
+
+        foreach ($deregister_styles as $style) {
+            Enqueue::deregisterStyle($style);
+        }
+
+        foreach ($deregister_scripts as $script) {
+            Enqueue::deregisterScript($script);
+        }
+    }
+
 }
