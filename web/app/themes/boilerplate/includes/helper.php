ac77c748f29d4c7e13ea689bbfb3ff1675da7ba2
Vasyl Sokolyk <vsokolyk@gmail.com>
2021-05-09 17:12:07 +0300

    removed extra functions

--- web/app/themes/boilerplate/includes/helper.php
+++ web/app/themes/boilerplate/includes/helper.php
@@ -1,42 +1,16 @@
 <?php
 
-if ( ! function_exists('is_rest')) {
-    /**
-     * @return bool
-     */
-    function is_rest()
+use Jaybizzle\CrawlerDetect\CrawlerDetect;
+
+if (!\function_exists('app_get_crawler_detect')) {
+    function app_get_crawler_detect(): CrawlerDetect
     {
-        $rest_prefix = trailingslashit(rest_get_url_prefix());
+        static $crawler;
 
-        return (false !== strpos($_SERVER['REQUEST_URI'], $rest_prefix));
-    }
-}
-
-
-if ( ! function_exists('app_add_attr_to_el')) {
-    /**
-     * @param array $attr
-     *
-     * @return string
-     */
-    function app_add_attr_to_el(array $attr)
-    {
-        $attributes = '';
-        foreach ($attr as $key => $value) {
-            if (empty($value)) {
-                continue;
-            }
-
-            if (is_array($value)) {
-                $value = implode(' ', array_filter($value));
-            }
-            if ('class' === $key && '' === $value) {
-                continue;
-            }
-
-            $attributes .= ' ' . esc_attr($key) . '="' . esc_attr($value) . '"';
+        if (null === $crawler) {
+            $crawler = new CrawlerDetect();
         }
 
-        return $attributes;
+        return $crawler;
     }
 }
