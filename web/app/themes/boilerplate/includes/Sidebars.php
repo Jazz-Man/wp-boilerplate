d55b039757c9d4ae51f3fa0b6354290be77903c6
Vasyl Sokolyk <vsokolyk@gmail.com>
2022-09-26 17:34:53 +0300

    update code style

--- web/app/themes/boilerplate/includes/Sidebars.php
+++ web/app/themes/boilerplate/includes/Sidebars.php
@@ -7,18 +7,15 @@ use JazzMan\AutoloadInterface\AutoloadInterface;
 /**
  * Class Sidebars.
  */
-class Sidebars implements AutoloadInterface
-{
-    public function load()
-    {
-        add_action('widgets_init', [$this, 'register_sidebars']);
+class Sidebars implements AutoloadInterface {
+    public function load() {
+        add_action('widgets_init', [__CLASS__, 'register_sidebars']);
     }
 
-    public function register_sidebars()
-    {
+    public static function register_sidebars() {
         register_sidebar([
-            'name'          => 'Sidebar',
-            'id'            => 'sidebar',
+            'name' => 'Sidebar',
+            'id' => 'sidebar',
         ]);
     }
 }
