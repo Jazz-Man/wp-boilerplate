e890e655b1b27e2ffd9d8f5f7edb84722c1073fc
Vasyl Sokolyk <vsokolyk@gmail.com>
2022-09-26 17:28:04 +0300

    load all classes in functions.php

--- web/app/themes/boilerplate/functions.php
+++ web/app/themes/boilerplate/functions.php
@@ -1,5 +1,13 @@
 <?php
 
-use JazzMan\Themes\App;
+use JazzMan\Themes\EnqueueScripts;
+use JazzMan\Themes\SetupTheme;
+use JazzMan\Themes\Sidebars;
 
-new App();
+if (function_exists('app_autoload_classes')) {
+    app_autoload_classes([
+        SetupTheme::class,
+        EnqueueScripts::class,
+        Sidebars::class,
+    ]);
+}
